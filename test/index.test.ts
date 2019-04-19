577efe92d44c05d3b56b0c7558994de7e02aba2a
vlikin <viktor.likin@gmail.com>
2019-04-19 14:19:42 +0300

    Ready for development.

---
+++ test/index.test.ts
@@ -0,0 +1,9 @@
+import {test} from '../lib/'
+import {expect} from 'chai';
+
+describe('test', () => {
+  it('test', () => {
+    const result = test();
+    expect(result).equal(result);
+  })
+});
